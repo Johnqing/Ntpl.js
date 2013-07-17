fb882789c945b994062a311de47bb16b58252018
刘卿 <csssnow@gmail.com>
2013-07-17 11:59:46 +0800

    Update nTpl.js

--- nTpl.js
+++ nTpl.js
@@ -8,7 +8,26 @@
 */
 (function(window, undefined){
     var NTpl = window.NTpl = window.NTpl || {},
-        doc = window.document;
+        doc = window.document,
+        vars = 'var ',
+        logicInTpl = {},
+        codeArr = ''.trim ?
+        ['ret = "";', 'ret +=', ';', 'ret;']
+        :['ret = [];', 'ret.push("', '")', 'ret.join("");'],
+        keys = ('break,case,catch,continue,debugger,default,delete,do,else,false,finally,for,function,if'
+            + ',in,instanceof,new,null,return,switch,this,throw,true,try,typeof,var,void,while,with'
+            // Reserved words
+            + ',abstract,boolean,byte,char,class,const,double,enum,export,extends,final,float,goto'
+            + ',implements,import,int,interface,long,native,package,private,protected,public,short'
+            + ',static,super,synchronized,throws,transient,volatile'
+            
+            // ECMA 5 - use strict
+            + ',arguments,let,yield').split(','),
+        keyMap = {};
+        
+    for (var i = 0, len = keys.length; i < len; i ++) {
+        keyMap[keys[i]] = 1;
+    }
 
     /**
      * 编译器
@@ -17,8 +36,22 @@
      * @private
      */
     var _compile  = function(str){
-        var fnBody = "var _t='',_tn,fn=(function(d){\n_tn='';\nfor(n in d){\n_tn+=('var '+n+'=d[\"'+n+'\"];');\n};\neval(_tn);\n_t +='"+_analysisStr(str)+"';\n_tn=null;\n})(tplObj);\nfn = null;\nreturn _t;";
-        return new Function('tplObj', fnBody);
+        var openArr = str.split(NTpl.openTag),
+            tmpCode = '';
+            
+        for (var i = 0, len = openArr.length; i < len; i ++) {
+            var c = openArr[i],
+                cArr = c.split(NTpl.closeTag);
+            if (cArr.length == 1) {
+                tmpCode += _html(cArr[0]);
+            } else {
+                tmpCode += _js(cArr[0]);
+                tmpCode += cArr[1] ? _html(cArr[1]) : '';
+            }
+        }
+    
+        var code = vars + codeArr[0] + tmpCode + 'return ' + codeArr[3];
+        return new Function('$data', '$getValue', code);
     };
 
     /**
@@ -42,7 +75,7 @@
             }
         })();
         //有数据则返回HTML字符串，没有数据则返回函数
-        var result = typeof data === 'object' ? fn( data ) : fn;
+        var result = typeof data === 'object' ? fn(data, getValue) : fn;
         fn = null;
         return result;
     };
@@ -50,30 +83,66 @@
      * 分隔符
      * @type {string}
      */
-    NTpl.leftDelimiter =  NTpl.leftDelimiter || '<%';
-    NTpl.rightDelimiter = NTpl.rightDelimiter || '%>';
+    NTpl.openTag =  NTpl.openTag || '<%';
+    NTpl.closeTag = NTpl.closeTag || '%>';
     /**
-     * 解析器
-     * @param str
-     * @returns {string} 返回模板
-     * @private
+     * html解析
+     * @param  {String} s 模板
+     * @return {String}   解析完成字符串
      */
-    var _analysisStr = function(str){
-        var lit = new RegExp("((^|"+NTpl.rightDelimiter+")[^\t]*)'","g"),
-            lit2 = new RegExp("\t=(.*?)"+NTpl.rightDelimiter,"g");
-        var tpl = str.replace(/[\r\t\n]/g, " ")
-            .replace(/=\s/g,'=')
-            .split(NTpl.leftDelimiter).join("\t")
-            .replace(lit, "$1\r")
-            .replace(lit2, "'+$1+'")
-            .split("\t")
-            .join("';")
-            .split(NTpl.rightDelimiter)
-            .join("_t += '")
-            .split("\r")
-            .join("\\'");
-        return tpl;
-    };
+    function _html (s) {
+        s = s
+        .replace(/('|"|\\)/g, '\\$1')
+        .replace(/\r/g, '\\r')
+        .replace(/\n/g, '\\n');
+        
+        s = codeArr[1] + '"' + s + '"' + codeArr[2];
+        
+        return s + '\n';
+    }
+    /**
+     * js解析
+     * @param  {String} s 模板
+     * @return {String}   解析完成字符串
+     */
+    function _js (s) {
+        /**
+         * 非js逻辑语句
+         */
+        if (/^=/.test(s)) {
+            s = codeArr[1] + s.substring(1).replace(/[\s;]*$/, '') + codeArr[2];
+        }
+        dealLogic(s);        
+        return s + '\n';
+    }
+    /**
+     * 处理js逻辑语句
+     * @param  {String} s 模板
+     */
+    function dealLogic (s) {
+        s = s.replace(/\/\*.*?\*\/|'[^']*'|"[^"]*"|\.[\$\w]+/g, '');
+        var sarr = s.split(/[^\$\w\d]+/);
+        // console.log(s);
+        for (var i = 0, len = sarr.length; i < len; i ++) {
+            var logic = sarr[i];
+            if (!logic || keyMap[logic] || /^\d/.test(logic)) {
+                continue;
+            }
+            if (!logicInTpl[logic]) {
+                vars += (logic + '= $getValue("' + logic + '", $data),');
+                logicInTpl[logic] = 1;
+            }
+        }
+    }
+    /**
+     * 数据解析
+     * @param  {String} v     key
+     * @param  {Object} $data 数据
+     * @return {String}       value
+     */
+    function getValue (v, $data){
+        return $data.hasOwnProperty(v) ? $data[v] : window[v];
+    }
     /**
      * 命名空间
      * @type {*}

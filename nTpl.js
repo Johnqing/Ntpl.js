eb0d29cd0e3ce9f3ab32a43c8e2d60823cd7aad8
johnqing <csssnow@gmail.com>
2014-01-22 16:34:16 +0800

    add error tips

--- nTpl.js
+++ nTpl.js
@@ -1,155 +1,235 @@
-/**
- * @author johnqing
- * @blog http://johnqing.github.io/
- * @param str{String} dom结点ID，或者模板string
- * @param data{Object} 需要渲染的json对象，可以为空。当data为{}时，仍然返回html。
- * @return 如果无data，直接返回编译后的函数；如果有data，返回html。
- * @up: 模板拼接改为字符串拼接，提升高版本浏览器模板拼接速度
-*/
-(function(window, undefined){
-    var NTpl = window.NTpl = window.NTpl || {},
-        doc = window.document,
-        vars = 'var ',
-        logicInTpl = {},
-        codeArr = ''.trim ?
-        ['ret = "";', 'ret +=', ';', 'ret;']
-        :['ret = [];', 'ret.push(', ')', 'ret.join("");'],
-        keys = ('break,case,catch,continue,debugger,default,delete,do,else,false,finally,for,function,if'
-            + ',in,instanceof,new,null,return,switch,this,throw,true,try,typeof,var,void,while,with'
-            // Reserved words
-            + ',abstract,boolean,byte,char,class,const,double,enum,export,extends,final,float,goto'
-            + ',implements,import,int,interface,long,native,package,private,protected,public,short'
-            + ',static,super,synchronized,throws,transient,volatile'
-            
-            // ECMA 5 - use strict
-            + ',arguments,let,yield').split(','),
-        keyMap = {};
-        
-    for (var i = 0, len = keys.length; i < len; i ++) {
-        keyMap[keys[i]] = 1;
+(function(globle, undefined){
+    // 防止重复调用
+    if(globle.NTpl){
+        return;
     }
+    if(!globle.exports){
+        globle.exports = {};
+    }
+    // 命名空间
+    var NT = globle.NTpl = globle.NT = exports.NT = {
+        openTag: '<%',
+        closeTag: '%>'
+    };
 
+    /*
+    * unit
+     */
+    var logicInTpl = {},
+        _cache = {},
+        vars = 'var ',
+        _isNewEngine = ''.trim,
+        codesArr = _isNewEngine ? ['ret = "";', 'ret +=', ';', 'ret;'] : ['ret = [];', 'ret.push(', ')', 'ret.join("");'];
+
+    // 数组迭代方法
+    var _forEach = Array.prototype.forEach || function (block, thisObject) {
+        var len = this.length >>> 0;
+
+        for (var i = 0; i < len; i++) {
+            if (i in this) {
+                block.call(thisObject, this[i], i, this);
+            }
+        }
+
+    };
+
+    // js关键字表
+    var _keyWordsMaps = {};
+
+    _forEach.call((
+        // 关键字
+        'break,case,catch,continue,debugger,default,delete,do,else,false,finally,for,function,if'
+        + ',in,instanceof,new,null,return,switch,this,throw,true,try,typeof,var,void,while,with'
+        // 保留字
+        + ',abstract,boolean,byte,char,class,const,double,enum,export,extends,final,float,goto'
+        + ',implements,import,int,interface,long,native,package,private,protected,public,short'
+        + ',static,super,synchronized,throws,transient,volatile'
+
+        // ECMA 5 - use strict
+        + ',arguments,let,yield'
+    ).split(','), function(key){
+        _keyWordsMaps[key] = true;
+    });
+    // 错误处理
+    function _debug(e){
+        var content = '[template]:\n'
+            + e.id
+            + '\n\n[name]:\n'
+            + e.name;
+
+
+        if(e.message){
+            content += '\n\n[message]:\n'
+                    + e.message;
+        }
+
+        if(e.line){
+            content += '\n\n[line]:\n'
+                    + e.line;
+        }
+
+
+        if(e.temp){
+            content += '\n\n[temp]:\n'
+                + e.temp;
+        }
+
+        if(globle.console){
+            console.error(content);
+        }
+    }
     /**
-     * 编译器
-     * @param str
-     * @returns {Function} 返回模板拼接函数
+     * 获取字符串缓存，没有则为设置
+     * @param {String} id
+     * @returns {String}
      * @private
      */
-    var _compile  = function(str){
-        var openArr = str.split(NTpl.openTag),
-            tmpCode = '';
-            
-        for (var i = 0, len = openArr.length; i < len; i ++) {
-            var c = openArr[i],
-                cArr = c.split(NTpl.closeTag);
-            if (cArr.length == 1) {
-                tmpCode += _html(cArr[0]);
-            } else {
-                tmpCode += _js(cArr[0]);
-                tmpCode += cArr[1] ? _html(cArr[1]) : '';
+    function _getCache(id){
+        var cache = _cache[id];
+
+        if(!cache){
+            var elem = document.getElementById(id);
+            if(elem){
+                exports.compile(id, elem.value || elem.innerHTML);
             }
+            return _cache[id];
         }
-    
-        var code = vars + codeArr[0] + tmpCode + 'return ' + codeArr[3];
-        return new Function('$data', '$getValue', code);
-    };
+        return cache;
+    }
 
     /**
-     * 对外接口函数
-     * @param str id
-     * @param data 数据
+     * 数据解析
+     * @param v
+     * @param $data
      * @returns {*}
      */
-    NTpl.tpl = function(str, data){
-        var fn = (function(){
-            var elem = doc.getElementById(str);
-            if(elem){
-                //缓存编译后的函数模板
-                if(nt.cache[str]){
-                    return nt.cache[str];
-                }
-                var html = /^(textarea|input)$/i.test(elem.nodeName) ? elem.value : elem.innerHTML;
-                return nt.cache[str] = _compile(html);
-            }else{
-                return _compile(str);
-            }
-        })();
-        //有数据则返回HTML字符串，没有数据则返回函数
-        var result = typeof data === 'object' ? fn(data, getValue) : fn;
-        fn = null;
-        return result;
-    };
-    /**
-     * 分隔符
-     * @type {string}
-     */
-    NTpl.openTag =  NTpl.openTag || '<%';
-    NTpl.closeTag = NTpl.closeTag || '%>';
-    /**
-     * html解析
-     * @param  {String} s 模板
-     * @return {String}   解析完成字符串
-     */
-    function _html (s) {
-        s = s
-        .replace(/('|"|\\)/g, '\\$1')
-        .replace(/\r/g, '\\r')
-        .replace(/\n/g, '\\n');
-        
-        s = codeArr[1] + '"' + s + '"' + codeArr[2];
-        
-        return s + '\n';
-    }
-    /**
-     * js解析
-     * @param  {String} s 模板
-     * @return {String}   解析完成字符串
-     */
-    function _js (s) {
-        /**
-         * 非js逻辑语句
-         */
-        if (/^=/.test(s)) {
-            s = codeArr[1] + s.substring(1).replace(/[\s;]*$/, '') + codeArr[2];
+    function getValue(v, $data){
+        try{
+            return $data.hasOwnProperty(v) ? $data[v] : globle[$data];
+        } catch (e){
+            return;
         }
-        dealLogic(s);        
-        return s + '\n';
+
     }
     /**
-     * 处理js逻辑语句
-     * @param  {String} s 模板
+     * js逻辑处理
+     * @param source
      */
-    function dealLogic (s) {
-        s = s.replace(/\/\*.*?\*\/|'[^']*'|"[^"]*"|\.[\$\w]+/g, '');
-        var sarr = s.split(/[^\$\w\d]+/);
-        // console.log(s);
-        for (var i = 0, len = sarr.length; i < len; i ++) {
-            var logic = sarr[i];
-            if (!logic || keyMap[logic] || /^\d/.test(logic)) {
-                continue;
+    function dealLogic(source){
+        source = source.replace(/\/\*.*?\*\/|'[^']*'|"[^"]*"|\.[\$\w]+/g, '');
+        var sArr = source.split(/[^\$\w\d]+/);
+
+        _forEach.call(sArr, function(logic){
+            if(!logic || _keyWordsMaps[logic] || /^\d/.test(logic)){
+                return;
             }
-            if (!logicInTpl[logic]) {
-                vars += (logic + '= $getValue("' + logic + '", $data),');
+            if(!logicInTpl[logic]){
+                vars += logic + '= $getValue("' + logic + '"),';
                 logicInTpl[logic] = 1;
             }
+        })
+    }
+    /**
+     * js|html 解析
+     * @param source
+     * @returns {string}
+     * @private
+     */
+    function _html(source){
+
+        source = source.replace(/('|"\\)/g, '\\$1')
+            .replace(/\r/g, '\\r')
+            .replace(/\n/g, '\\n');
+
+        source = codesArr[1] + '"' + source + '"' + codesArr[2];
+
+        return source + '\n';
+    }
+    function _js(source){
+
+        if(/^=/.test(source)){
+            source = codesArr[1] + source.substring(1).replace(/[\s;]*$/, '') + codesArr[2];
+        }
+
+        dealLogic(source)
+
+        return source;
+    }
+    /**
+     * 解析模板
+     * @param source
+     * @returns {Function}
+     * @private
+     */
+    function _compile(source){
+
+        var openArr = source.split(NT.openTag),
+            tempCode = '';
+
+        _forEach.call(openArr, function(code){
+            var codeArr = code.split(NT.closeTag);
+            var c0 = codeArr[0],
+                c1 = codeArr[1];
+            if(codeArr.length == 1){
+                tempCode += _html(c0);
+            } else{
+
+                tempCode += _js(c0);
+                tempCode += c1 ? _html(c1) : '';
+            }
+        });
+
+
+        var code = 'function $getValue(key){return $data.hasOwnProperty(key) ? $data[key] : this[$data];};'
+            + vars + codesArr[0] + tempCode + 'return '+ codesArr[3];
+        try{
+            return new Function('$data', code);
+        } catch (e){
+
         }
     }
+
     /**
-     * 数据解析
-     * @param  {String} v     key
-     * @param  {Object} $data 数据
-     * @return {String}       value
+     *  globle
      */
-    function getValue (v, $data){
-        return $data.hasOwnProperty(v) ? $data[v] : window[v];
+    exports.compile = function(id, source){
+        // 只传入字符串
+        if(typeof source != 'string'){
+            source = id;
+            id = null
+        }
+
+
+        try{
+            var cache = _compile(source);
+        } catch (e){
+            // 出错时，定位行数和内容
+            e.id = e.id || source;
+            e.name = 'Syntax Error';
+            return _debug(e);
+        }
+
+        if(id){
+            _cache[id] = cache;
+        }
+        //console.log(cache);
+        return cache;
+
     }
-    /**
-     * 命名空间
-     * @type {*}
-     */
-    var nt = NTpl.tpl;
-    /**
-     * 缓存
-     */
-    nt.cache = {};
-}(this));
+
+    // 接口
+    NT.tpl = function(id, data, settings){
+        var cache = _getCache(id);
+
+        if(cache === undefined){
+            return _debug({
+                id: id,
+                name: 'Rend Error',
+                message: 'Not get template'
+            });
+        }
+        return data ? cache(data) : cache;
+    }
+
+
+})(this);
